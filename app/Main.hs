c79754ca8fdef384248feb357053c64c3d67c4c9
gareth <g@betechnology.com.au>
2017-05-16 11:11:49 +1000

    prototype usage of the state monad

---
+++ app/Main.hs
@@ -0,0 +1,40 @@
+{- Carls challenge
+ - -------------------------
+ -
+ - A simple example of using the state monad combined with 
+ - taking input from io
+ -
+ - ask the user for an integer input, when received then 
+ - increment the counter in memory, display the value
+ - and then ask the user for more input. a
+ -
+ -}
+
+module Main where
+
+import Control.Monad.State
+
+type App = StateT Integer IO
+
+askForNumber :: App ()
+askForNumber = do
+  -- read input from IO
+  i <- liftIO readLn :: App Integer
+
+  -- get the state from the monad
+  counter <- get
+
+  -- increment
+  let value = counter + i
+
+  -- display to the user
+  liftIO $ putStrLn (show value)
+
+  -- override the state with the new value
+  put value
+
+  -- goto 20
+  askForNumber
+
+main :: IO ((), Integer)
+main = runStateT askForNumber 0
